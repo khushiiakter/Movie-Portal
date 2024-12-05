85a41ad3c01e1e3b468f15fba0834e4e3361ce02
khushiiakter <rkjia1311@gmail.com>
2024-12-06 03:03:01 +0600

    added single movie card design

---
+++ src/components/FeatureMovies.jsx
@@ -0,0 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+import Movie from "../components/Movie";
+
+const FeatureMovies = () => {
+  const movies = useLoaderData();
+  return (
+    <div>
+      <h1>Movies {movies.length}</h1>
+      <div className="grid mt-8
+       md:grid-cols-3 grid-cols-1 gap-4">
+        {movies.map((movie) => (
+          <Movie key={movie._id} movie={movie}></Movie>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeatureMovies;
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,13 +1,41 @@
+const Movie = ({ movie }) => {
+  const { rating, summary, year, duration, genre, title, poster, _id } = movie;
+  return (
+    <div className="  text-white rounded-2xl shadow-md overflow-hidden">
+      {/* Movie Poster */}
+      <div className="relative ">
+        <img className="w-full h-[470px] object-cover" src={poster} alt={title} />
 
-
-const Movie = ({movie}) => {
-    const {rating, summary, year, duration, genre, title, poster, _id} = movie;
-    return (
-        <div>
-            <h1>{title}</h1>
-            <h1>{rating}</h1>
+        <div className="flex items-center absolute bottom-[0px] pl-5 pb-4">
+          {/* Title and Rating */}
+          <h3 className="text-3xl  font-bold ">{title}</h3>
+          {/* Rating Number */}
+          <span className="ml-2 font-medium text-red-400 text-base">({rating})</span>
         </div>
-    );
+      </div>
+      
+
+      {/* Movie Info */}
+      <div className="py-4 bg-gray-900 px-5 flex  items-center justify-between">
+        
+
+        {/* Additional Info */}
+        
+        <div className="flex gap-3 ">
+          <p className="text-sm text-gray-400">{genre}</p>
+          <p className="text-sm text-gray-400 border-gray-400 border-x px-2">
+            {duration} min
+          </p>
+          <p className="text-sm text-gray-400">{year}</p>
+        </div>
+
+        {/* See Details Button */}
+        <button className="  px-4 py-2 bg-red-600 hover:bg-red-700 rounded-lg text-sm">
+          See Details
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Movie;
